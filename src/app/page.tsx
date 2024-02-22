e283ab76ce30406775ffe12eb3b30a7ebee937fb
SamanjaCartagena <cart.samanja09@gmail.com>
2024-02-22 10:16:08 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {NavigationMenuDemo} from '../components/ui/navigation'
+
+export default function Home() {
+  return (
+   <main>
+
+  <NavigationMenuDemo/>
+   </main>
+  );
+}
